20fde449bd654a370f46980c32b9138e365495dd
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-01-20 15:26:55 +0530

    hi

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const productRoute = require("./router/product-router")
 const checkOut = require("./router/checkout-router.js")
 const app = express();
 const connectDb = require("./utils/db");
-const PORT =    process.env.PORT||8000;
+const PORT =  process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json())
-
 
 app.use("/api/products",productRoute);
 app.use("/api/checkout",checkOut);
