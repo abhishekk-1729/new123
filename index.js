0ceef53c99247bd8c70566f7c00cff13a108262f
abhishekk-1729 <135116930+abhishekk-1729@users.noreply.github.com>
2024-01-21 19:42:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT =  process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json())
 
+app.use("/",(req,res) => {res.json({"message":"hi"})});
 
 app.use("/api/products",productRoute);
 app.use("/api/checkout",checkOut);
